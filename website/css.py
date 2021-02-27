0addae92917a350b9819c13581c1f8357912a8c6
debjanihazra <debjanihazra2001>
2021-02-27 05:07:28 +0000

    add css.py

---
+++ website/css.py
@@ -0,0 +1,36 @@
+body {
+    font-size: 15pt;
+    background-color: grey;
+    font-family: Arial;
+    color: white;
+}
+.welcome{
+  color: deepskyblue;
+  font-size: 30px;
+  font-style: italic;
+  
+}
+.top span{
+  color: pink;
+  font-style: italic;
+}
+.info{
+  border: 2px solid darkred;
+  padding: 20px;
+}
+b{ color: yellow;}
+img{
+  width: 80%;
+}
+.webpage{
+  color: hotpink
+}
+.myself{
+  color: palegoldenrod
+}
+.star{
+  color: pink
+}
+.p{
+  color: black
+}
