d708cb718c2e6536fd4a9af6dadfab72bc2097d1
debjanihazra <debjanihazra2001>
2021-02-27 04:57:53 +0000

    add html.py

---
+++ Problem-solving/question-1.py
@@ -0,0 +1,15 @@
+# fn(arg) -> num
+
+def addNum(number):
+    addition = 0
+    for i in range(1, n + 1):
+        addition += i
+        return addition
+
+
+
+    result = addNum(5)
+
+    print(result)
+
+
---
+++ website/html.py
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>PERSONAL WEBSITE</title>
+  
+  <!-- HTML -->
+  
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <center>
+    <h1 class="welcome">Welcome to my WEBSITE</h1>
+    <h3 class="top">I am <span>"Debjani Hazra"</span></h3>
+    <img src="photo.jpg">
+    <h1 class="myself"><i>About myself</i></h1>
+  
+  <p> Hello everyone!This is Debjani Hazra.I am from Kolkata chapter.I am persuing 5th sem from F C College in Diamond harbour.I am learning to make a beautiful webpage using coding.If you like my page, please subscribe. Thank you!!</p>
+  <br /><br />
+  <h1><i><u>Now let's know about<b> "coding"</b>:-</u></i></h1>
+  <p> Coding, sometimes called computer programming, is how we communicate with computers. Code tells a computer what actions to take, and writing code is like creating a set of instructions. By learning to write code, you can tell computers what to do or how to behave in a much faster way. You can use this skill to make websites and apps, process data, and do lots of other cool things. </p>
+  <img src="Kids-photo.png">
+  <h2><u><i>Why learn how to code?</i></u></h2>
+  <p> 
+  We all have hopes, dreams, and plans for the future. Whether you’re looking for a new opportunity, want to optimize your current job, or are simply searching for a new hobby, coding can help you get closer to your goals. And remember, anyone can learn how to code!</p>
+  <a href=" https://grasshopper.app/why-coding/#:~:text=What%20is%20coding%3F,in%20a%20much%20faster%20way. ">Know more.</a>
+  <h2 class="webpage"><u>Now let's see what is the <i>'webpage':-</i></u></h2>
+  <img src=" https://upload.wikimedia.org/wikipedia/commons/8/87/United_States_Antarctic_Program_website_from_2018_02_22.png ">
+  <p> A web page (or webpage) is a specific collection of information provided by a website and displayed to a user in a web browser. A website typically consists of many web pages linked together in a coherent fashion. The name "web page" is a metaphor of paper pages bound together into a book. </p>
+  <h1 class="star">*   *   *   *</h1>
+<h2><i><u>This is my github account. You can see my codes:-</u></i></h2>
+<a href="https://www.github.com/debjanihazra">
+  <img src="github.png" alt="github" />
+  <p class="p">I hope you may like this.I am very happy to see you.</p>
+ 
+    
+  <h1>Contact us</h1>
+  <div class="info">
+    <b>Name: <b/>Debjani Hazra
+    <br /
+    <b>E-mail address: <b/>debjanihazra2001@gmail.com
+    
+    
+  <br /><br />
+ 
+    <b>Date of birth: <b/>13/03/2001
+    <br /
+    <b>Phone: <b/>8170909757
+   
+      <h2>This is my twitter site.There is no any speciality, but you can tweet me.</h2>
+  <a href="https://www.twitter.com/debjanihazra3">
+    <img src=" twitter.png " alt="twitter">
+    
+  </center>
+  <center>
+  <p>Thank You!!</p>
+  </center>
+  <!-- Project -->
+  <script src="main.js"></script>
+</body>
+</html>
